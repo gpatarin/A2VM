0224e9695838bbe94607a8264dc6f53e051a63c6
Gael Patarin <gael.patarin@outlook.com>
2018-11-14 10:04:20 +0100

    Listvhost function operable

--- a2vm.sh
+++ a2vm.sh
@@ -5,14 +5,23 @@ function newVhost(){
 }
 
 function deleteVhost(){
-    echo "$1"
+    listVhost
+    read -ep "Select the site you want to delete: " -i Default answer ANSWER
+    echo "$ANSWER"
 }
 
 function listVhost(){
+    clear
     var=$(ls /etc/apache2/sites-available/ | grep ^000 | grep -v \\.save$ | sed -e 's/000-\(.*\)\.conf/\1/')
-    for i in var; do
-        echo "$1"
+    echo "List of available sites"
+    echo "======================="
+    count=0
+    for i in $var; do
+        count=$((count+1))
+        echo "$count:$i"
     done
+    echo "======================="
 }
 
 listVhost
+deleteVhost
