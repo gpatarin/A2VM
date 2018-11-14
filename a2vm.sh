496f2e2be988dd2e4d38c761916e6dd8644ae7a3
Gael Patarin <gael.patarin@outlook.com>
2018-11-14 09:28:07 +0100

    New function : listVhost to list Sites Available

--- a2vm.sh
+++ a2vm.sh
@@ -7,3 +7,12 @@ function newVhost(){
 function deleteVhost(){
     
 }
+
+function listVhost(){
+    var=$(ls /etc/apache2/sites-available/ | grep ^000 | grep -v \\.save$ | sed -e 's/000-\(.*\)\.conf/\1/')
+    for i in var; do
+        echo "$1"
+    done
+}
+
+listVhost
