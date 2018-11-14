6a132d0746a5c8b008b81e849559f26caea984cb
Gael Patarin <gael.patarin@outlook.com>
2018-11-14 21:07:29 +0100

    Adjustments

--- a2vm.sh
+++ a2vm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 N_SITES=0
+SITES_AVAILABLE=$(ls /etc/apache2/sites-available/ | grep ^000 | grep -v \\.save$ | sed -e 's/000-\(.*\)\.conf/\1/')
 
 function newVhost(){
     echo "$1"
@@ -8,11 +9,10 @@ function newVhost(){
 function deleteVhost(){
     listVhost
     selection=-1
-    while ((selection<0 || selection>N_SITES || selection==0))
+    while [[ selection -lt 0 ]] || [[ selection -gt N_SITES ]] || [[ selection -eq 0 ]]
     do
         read -p "Select the site you want to delete (type exit if you want to go back): " selection
-        if ((selection=="exit"))
-        then 
+        if [[ selection -eq "exit" ]];then
             break
         fi
     done
@@ -21,16 +21,15 @@ function deleteVhost(){
 
 function listVhost(){
     clear
-    var=$(ls /etc/apache2/sites-available/ | grep ^000 | grep -v \\.save$ | sed -e 's/000-\(.*\)\.conf/\1/')
-    echo "List of available sites"
-    echo "======================="
+    echo "List of available sites apache"
+    echo "==============================="
     count=0
-    for i in $var; do
+    for i in ${SITES_AVAILABLE}; do
         count=$((count+1))
         echo "$count:$i"
     done
-    N_SITES=$count
-    echo "======================="
+    N_SITES=${count}
+    echo "==============================="
 }
 
 listVhost
