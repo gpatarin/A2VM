31188b290c8b8423a144111032a2930d000e1bdd
gpatarin <gael.patarin@etu.univ-tlse2.fr>
2018-11-16 21:28:15 +0100

    Reset of the main bash script to follow a real developpement guideline

--- a2vm.sh
+++ a2vm.sh
@@ -1,50 +1,5 @@
 #!/bin/bash
-N_SITES=0
-SITES_AVAILABLE=$(ls /etc/apache2/sites-available/ | grep ^000 | grep -v \\.save$ | sed -e 's/000-\(.*\)\.conf/\1/')
-
-for i in ${SITES_AVAILABLE}; do
-    LIST_OF_SITES=("${LIST_OF_SITES[@]}" i)
-done
 
 function newVhost(){
     echo "$1"
 }
-
-function deleteVhost(){
-    listVhost
-    selection=-1
-    while [[ selection -lt 0 ]] || [[ selection -gt N_SITES ]] || [[ selection -eq 0 ]];
-    do
-        read -p "Select the site you want to delete (type exit if you want to go back): " selection
-
-        if [[ selection -eq 0 ]]; then
-            selection = -1
-        fi
-
-
-        if [[ selection -eq "exit" ]];then
-            echo "exited"
-            break
-        fi
-    done
-
-
-    echo "Selected $LIST_OF_SITES[$selection]"
-
-}
-
-function listVhost(){
-    clear
-    echo "List of available sites apache"
-    echo "==============================="
-    count=0
-    for i in ${SITES_AVAILABLE}; do
-        count=$((count+1))
-        echo "$count:$i"
-    done
-    N_SITES=${count}
-    echo "==============================="
-}
-
-listVhost
-deleteVhost
